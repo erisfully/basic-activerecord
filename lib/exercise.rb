558d2946208cc8778cc2783031b7ee029cd2db0f
Ashley <a.clarkem@gmail.com>
2014-07-28 14:57:42 -0600

    join orders and customers part 3

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -6,4 +6,4 @@ require "./lib/order"
 require "awesome_print"
 
 
-ap Order.joins(:customer).select('customers.id, customers.name, sum(amount)').group('name, customers.id').order("name ASC").to_sql
+ap Order.joins(:customer).select('customers.id, customers.name, avg(amount)').group('name, customers.id').order("name ASC")
